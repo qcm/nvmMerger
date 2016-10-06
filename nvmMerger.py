04ba1027d6d13b457265b61756a54833801a970b
Bo-Yi Lee <boyil@qca.qualcomm.com>
2016-10-05 18:00:15 -0700

    initial version

---
+++ nvmMerger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import argparse
+
+def optParser():
+	print "this is optParser"
+	parser = argparse.ArgumentParser(description='Process input')
+	
+	
+def nvmMerger():
+	optParser()
+
+nvmMerger()
