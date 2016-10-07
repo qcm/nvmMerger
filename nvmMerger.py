b57baf94722b2277e1eaa869890fd6c94f9e15bb
Bo-Yi Lee <boyil@qca.qualcomm.com>
2016-10-06 18:19:23 -0700

    Completes the input parser

--- nvmMerger.py
+++ nvmMerger.py
@@ -2,9 +2,14 @@
 import argparse
 
 def optParser():
-	print "this is optParser"
-	parser = argparse.ArgumentParser(description='Process input')
+	parser = argparse.ArgumentParser(description='nvmMerger merges two NVM bin files into one')
+	parser.add_argument('-f', metavar='bin1', type=file, required=True, help='First bin file, the base NVM')
+	parser.add_argument('-s', metavar='bin2', type=file, required=True, help='Second bin file to add')
+	parser.add_argument('-m', metavar='bin3', type=str, required=True, help='Merged bin file')
+	#parser.print_help()
 	
+	args = parser.parse_args()
+	print args
 	
 def nvmMerger():
 	optParser()
