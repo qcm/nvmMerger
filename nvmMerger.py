46626dfb4683f0e67ca7337f1596a1af7f7bd74b
Bo-Yi Lee <boyil@qca.qualcomm.com>
2016-10-10 12:12:22 -0700

    Finish fill out the NVM class from file

--- nvmMerger.py
+++ nvmMerger.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 import argparse
+import binascii
+
+NVM_TLV_DATA_START = 4
+NVM_TLV_ZERO_PADDING = 8
+
+# create lists stores whole bin file
+list_f = []
+list_s = []
 
 def optParser():
 	parser = argparse.ArgumentParser(description='nvmMerger merges two NVM bin files into one')
@@ -14,18 +22,58 @@ def optParser():
 	
 	return args
 	
+class NVMTag:
+	def __init__(self, TNL, TNB, TLL, TLM):
+		self.TagNumLSB = TNL  
+		self.TagNumMSB = TNB
+		self.TagLengthLSB = TLL
+		self.TagLengthMSB = TLM
+		self.length = 0
+		self.TagValue = []
+	
+	def inputval(self, fobj):
+		iLSB = int(binascii.b2a_hex(self.TagLengthLSB), 16)
+		iMSB = int(binascii.b2a_hex(self.TagLengthMSB), 16)
+		self.length = iLSB + iMSB*16
+		#print num
+		for i in range(self.length):
+			x = fobj.read(1)
+			self.TagValue.append(x)
+			i += 1
+
+	
+	def printall(self):
+		print binascii.b2a_hex(self.TagNumLSB)
+		print binascii.b2a_hex(self.TagNumMSB)
+		print binascii.b2a_hex(self.TagLengthLSB)
+		print binascii.b2a_hex(self.TagLengthMSB)
+		for i in range(self.length):
+			print binascii.b2a_hex(self.TagValue[i])
+			i += 1
+
+def bin2list(fobj, nvm_list):
+	# Move cursor to where data starts
+	fobj.seek(NVM_TLV_DATA_START)
+	t1 = NVMTag(fobj.read(1), fobj.read(1), fobj.read(1), fobj.read(1))
+	fobj.seek(NVM_TLV_ZERO_PADDING, 1)
+	t1.inputval(fobj)
+	#t1.printall()
+	
+
 def nvmMerger():
 	args = optParser()
 	m = open(args.m, 'w+b')
 	f = open(args.f, 'rb+')
+	s = open(args.s, 'rb+')
 	#f.readline()
 	#m.write(f.readline())
-	for line in f:
-		print line
-	
+	#for line in f:
+	#	print line
+	#
+	#i = int.from_bytes(f.read(1), byteorder='big') # only valid in Python3
+	bin2list(f, list_f)
+
 	m.close()
 	f.close()
 
-#class NVMTag:
-	
 nvmMerger()
