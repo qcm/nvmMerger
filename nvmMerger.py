e6f311a89b82b054a0fd181d86fd4ef1d2c61828
Bo-Yi Lee <b91505015@gmail.com>
2016-10-10 04:22:31 -0700

    Correct the add_argument type from 'file' to 'str'

--- nvmMerger.py
+++ nvmMerger.py
@@ -3,8 +3,8 @@ import argparse
 
 def optParser():
 	parser = argparse.ArgumentParser(description='nvmMerger merges two NVM bin files into one')
-	parser.add_argument('-f', metavar='bin1', type=file, required=True, help='First bin file, the base NVM')
-	parser.add_argument('-s', metavar='bin2', type=file, required=True, help='Second bin file to add')
+	parser.add_argument('-f', metavar='bin1', type=str, required=True, help='First bin file, the base NVM')
+	parser.add_argument('-s', metavar='bin2', type=str, required=True, help='Second bin file to add')
 	parser.add_argument('-m', metavar='bin3', type=str, required=True, help='Merged bin file')
 	#parser.print_help()
 	
@@ -17,8 +17,14 @@ def optParser():
 def nvmMerger():
 	args = optParser()
 	m = open(args.m, 'w+b')
-	f = open('memtest86+.bin', 'rb+')
-	f.readline()
-	m.write(f.readline())
+	f = open(args.f, 'rb+')
+	#f.readline()
+	#m.write(f.readline())
+	for line in f:
+		print line
+	
 
+
+#class NVMTag:
+	
 nvmMerger()
